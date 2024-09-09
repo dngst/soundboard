7671866ca40e011c28b096bd301a83991a232710
dngst <dngst@protonmail.com>
2024-09-09 21:23:07 +0300

    Add sound grid

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,51 @@
-const audioElement = document.getElementById("audio");
-const playPauseButton = document.getElementById("player");
+$(document).ready(function() {
+    const defaultTitle = document.title;
+    const audioFiles = [
+        "sounds/gotem.mp3",
+        "sounds/wow.mp3",
+        "sounds/yeet.mp3",
+        "sounds/laugh.mp3",
+        "sounds/nani.mp3",
+        "sounds/tom.mp3",
+        "sounds/windows.mp3",
+        "sounds/loading.mp3",
+        "sounds/aughhhhh.mp3",
+        "sounds/milk.mp3",
+    ];
+    const soundGrid = $("#sound-grid");
 
-if (audioElement && playPauseButton) {
-    playPauseButton.addEventListener("click", function() {
-        if (audioElement.paused) {
-            audioElement.play();
-            playPauseButton.innerHTML = "&#10074;&#10074;";
-        } else {
-            audioElement.pause();
-            playPauseButton.innerHTML = "&#9658;";
-        }
+    audioFiles.forEach((audioFile, index) => {
+        const fileTitle = audioFile.split("/")[1].split(".")[0];
+        const audioPlayer = `
+            <div class="player">
+                <audio id="sound-${index}">
+                    <source src="${audioFile}" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+                <div>${fileTitle}</div>
+            </div>
+        `;
+
+        soundGrid.append(audioPlayer);
+
+        const $player = $(`.player:eq(${index})`);
+        const audio = $player.find('audio')[0];
+
+        $player.click(() => {
+            if (audio.paused) {
+                audio.play();
+                document.title = fileTitle;
+                document.body.style.cursor = 'wait';
+            } else {
+                audio.pause();
+                document.body.style.cursor = 'default';
+            }
+        });
+
+        audio.addEventListener('ended', () => {
+            document.title = defaultTitle;
+            document.body.style.cursor = 'default';
+        });
     });
-
-    audioElement.addEventListener("ended", function() {
-        playPauseButton.innerHTML = "&#9658;";
-    });
-
-} else {
-    console.error("Audio or player element not found.");
-}
+});
 
